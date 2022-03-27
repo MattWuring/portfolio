425d8cc9670161f4e87464e2212d3b173abd4f35
Matt Wuring <matt.wuring@gmail.com>
2022-03-27 23:15:50 +0200

    navbar and showcase fix

--- script/script.js
+++ script/script.js
@@ -1,32 +1,3 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("Slides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-
 function navbar(){
   var x = document.getElementById("toggle-navbar");
   if (x.style.display === "block") {
@@ -35,7 +6,3 @@ function navbar(){
       x.style.display = "block";
   }
 }
-
-// if (window.matchMedia("(min-width: 900px)").matches) {
-//   document.getElementById("toggle-navbar").style.display = "none";
-// }
